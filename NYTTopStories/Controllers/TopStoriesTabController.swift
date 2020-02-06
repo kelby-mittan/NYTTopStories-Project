9381682a7f9cc86fbaf023b00cb43bb11d4446fc
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-06 10:49:27 -0500

    Initial Commit

---
+++ NYTTopStories/Controllers/TopStoriesTabController.swift
@@ -0,0 +1,20 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Kelby Mittan on 2/6/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemTeal
+    }
+    
+    
+}
