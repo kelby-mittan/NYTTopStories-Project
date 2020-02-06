ea245f204c609063af33a8211667f64ae52e5270
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-06 12:43:40 -0500

    created API Client

--- NYTTopStories/APIClients/NYTTopStoriesAPIClient.swift
+++ NYTTopStories/APIClients/NYTTopStoriesAPIClient.swift
@@ -7,5 +7,34 @@
 //
 
 import Foundation
+import NetworkHelper
 
-
+struct NYTAPIClient {
+    static func getPix(for city: String, completion: @escaping (Result<TopStories, AppError>) -> ()) {
+        
+        
+        let NYTEndpointURL = ""
+        
+        guard let url = URL(string: NYTEndpointURL) else {
+            completion(.failure(.badURL(NYTEndpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    let pixHits = topStories
+                    completion(.success(pixHits))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
