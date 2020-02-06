250c9a7eca992837e50b6e14c6b1b8a9b7c44b91
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-06 12:34:58 -0500

    made model and successful unit test

---
+++ NYTTopStories/Models/TopStory.swift
@@ -0,0 +1,46 @@
+//
+//  TopStory.swift
+//  NYTTopStories
+//
+//  Created by Kelby Mittan on 2/6/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import Foundation
+
+
+struct TopStories: Codable {
+    let section: String
+    let lastUpdated: String
+    let results: [Article]
+    
+    private enum CodingKeys: String, CodingKey {
+        case section
+        case lastUpdated = "last_updated"
+        case results
+    }
+    
+}
+
+struct Article: Codable {
+    let section: String
+    let title: String
+    let abstract: String
+    let publishedDate: String
+    let multimedia: [Multimedia]
+    private enum CodingKeys: String, CodingKey {
+        case section
+        case title
+        case abstract
+        case publishedDate = "published_date"
+        case multimedia
+    }
+}
+
+struct Multimedia: Codable {
+    let url: String
+    let format: String
+    let height: Double
+    let width: Double
+    let caption: String
+}
